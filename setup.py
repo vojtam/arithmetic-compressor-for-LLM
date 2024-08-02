cd652f0c378584fd5f62c4e7c1a0856a3c0983b2
Vojtěch Máčala <macala.vojtech@gmail.com>
2024-08-02 12:05:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='An implementation of the Arithmetic Coding algorithm in Python, along with advanced models like PPM (Prediction by Partial Matching), Context Mixing and Simple Adaptive models',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/kodejuice/arithmetic-compressor',
+    url='https://github.com/vojtam/arithmetic-compressor-for-LLM',
     packages=find_packages(),
     test_suite='unittest.TestLoader().discover("tests")',
     classifiers=[
