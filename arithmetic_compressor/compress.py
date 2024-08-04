d5706864de849f23355a1104f3518414c3b17140
vojtam <macala.vojtech@gmail.com>
2024-08-04 16:37:01 +0200

    feat: tqdm description

--- arithmetic_compressor/compress.py
+++ arithmetic_compressor/compress.py
@@ -15,7 +15,7 @@ class AECompressor:
   def compress(self, data):
     encoder = AE.Encoder()
 
-    for symbol in tqdm(data):
+    for symbol in tqdm(data, desc = "Compression"):
       # Use the model to predict the probability of the next symbol
       cdf = self.model.cdf()
 
@@ -32,7 +32,7 @@ class AECompressor:
     decoded = []
     model = self.__model
     decoder = AE.Decoder(encoded)
-    for _ in tqdm(range(length_encoded)):
+    for _ in tqdm(range(length_encoded), desc = "Decompression"):
       # probability of the next symbol
       probability = model.cdf()
 
